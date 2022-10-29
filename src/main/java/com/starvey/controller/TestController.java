cb38898b50b028a9f1f6efaaf8779c10ba935c37
Song <72328819+szy1840@users.noreply.github.com>
2022-10-29 11:19:05 +0800

    chore: 配置了swagger3并修正了逻辑删除的部分 (#3)
    * chore: 配置了swagger3
    
    * fix: 更新了sql文件以实现逻辑删除，修正了test controller

--- src/main/java/com/starvey/controller/TestController.java
+++ src/main/java/com/starvey/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.starvey.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.starvey.common.Result;
 import com.starvey.entity.User;
 import com.starvey.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +15,21 @@ public class TestController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation("测试接口")
     @GetMapping("/test")
     public Result test() {
         System.out.println("/test 访问成功");
         User user = new User();
-        user.setId("1");
+        String id = IdUtil.randomUUID();
+        user.setId(id);
         userService.save(user);
-        for (User user1 : userService.list()) {
-            System.out.println(user1);
-        }
-
-        userService.removeById("1");
 
         for (User user1 : userService.list()) {
             System.out.println(user1);
         }
+
+        userService.removeById(id);
+
         return  Result.success(user);
     }
 }
---
+++ src/main/resources/starvey.sql
@@ -0,0 +1,138 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : localhost:3306
+ Source Schema         : questionnaire
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 29/10/2022 11:04:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for answer
+-- ----------------------------
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `question_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `questionnaire_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `question_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `question_description` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
+  `order` int NULL DEFAULT NULL,
+  `answer_content` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of answer
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for answer_sheet
+-- ----------------------------
+DROP TABLE IF EXISTS `answer_sheet`;
+CREATE TABLE `answer_sheet`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `questionnaire_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `fill_time` datetime NULL DEFAULT NULL,
+  `is_deleted` int NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of answer_sheet
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question
+-- ----------------------------
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `questionnaire_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `order` int NULL DEFAULT NULL,
+  `is_necessary` tinyint(1) NULL DEFAULT NULL,
+  `question_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `question_description` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of question
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for questionnaire
+-- ----------------------------
+DROP TABLE IF EXISTS `questionnaire`;
+CREATE TABLE `questionnaire`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `tenant_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
+  `status` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `question_num` int NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `start_time` datetime NULL DEFAULT NULL,
+  `end_time` datetime NULL DEFAULT NULL,
+  `count_limit` int NULL DEFAULT NULL,
+  `fill_count` int NULL DEFAULT NULL,
+  `is_deleted` int NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of questionnaire
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tenant
+-- ----------------------------
+DROP TABLE IF EXISTS `tenant`;
+CREATE TABLE `tenant`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `cost` double(10, 2) NULL DEFAULT NULL,
+  `is_deleted` int NULL DEFAULT 0
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of tenant
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `tenant_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `phone_num` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `role` int NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `is_deleted` int NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('392cc2a2-4e8e-4b25-8c1f-915dd6078f6e', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
+INSERT INTO `user` VALUES ('dbe3f043-98f1-4031-a73c-f42800783c0a', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
