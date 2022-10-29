3c5f40149d67c252f5269b1248c41fab2437bffb
Song <72328819+szy1840@users.noreply.github.com>
2022-10-29 21:51:02 +0800

    添加问卷CRUD接口，并修改了一处sql (#7)
    * feat: 添加后端问卷CRUD接口
    
    * fix: 修改问卷类型属性默认为NULL
    
    * Delete maven.yml

--- src/main/java/com/starvey/common/Result.java
+++ src/main/java/com/starvey/common/Result.java
@@ -18,6 +18,10 @@ public class Result implements Serializable {
         return success(200, "操作成功", data);
     }
 
+    public static Result success(String msg, Object data) {
+        return success(200, msg, data);
+    }
+
     public static Result success(int code, String msg, Object data) {
         Result r = new Result();
         r.setCode(code);
---
+++ src/main/java/com/starvey/controller/QuestionnaireController.java
@@ -0,0 +1,53 @@
+package com.starvey.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.starvey.common.Result;
+import com.starvey.entity.Questionnaire;
+import com.starvey.service.QuestionnaireService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@Api(tags = "问卷管理")
+@RestController
+public class QuestionnaireController {
+    @Autowired
+    private QuestionnaireService questionnaireService;
+
+    @ApiOperation("获取指定页大小下的指定页的问卷列表")
+    @GetMapping("/questionnaire/list")
+    public Result getQuestionnaires(@RequestParam(defaultValue = "1") Integer pageNumber, Integer pageSize) {
+        Page<Questionnaire> page = questionnaireService.page(new Page<>(pageNumber, pageSize));
+        return Result.success(page);
+    }
+
+    @ApiOperation("获取指定id的问卷")
+    @GetMapping("/questionnaire/{id}")
+    public Result getQuestionnaire(@PathVariable(name = "id") String id) {
+        Questionnaire questionnaire = questionnaireService.getById(id);
+        return questionnaire != null ? Result.success(questionnaire) : Result.fail("获取指定问卷失败");
+    }
+
+    @ApiOperation("添加问卷")
+    @PostMapping("/questionnaire/add")
+    public Result addQuestionnaire(@RequestBody Questionnaire questionnaire) {
+        boolean b = questionnaireService.save(questionnaire);
+        return b ? Result.success(questionnaire) : Result.fail("添加问卷失败");
+    }
+
+    @ApiOperation("更新指定id的问卷")
+    @PostMapping ("/questionnaire/update")
+    public Result updateQuestionnaire(@RequestBody Questionnaire questionnaire) {
+        boolean b = questionnaireService.updateById(questionnaire);
+        return b ? Result.success(questionnaire) : Result.fail("更新指定问卷失败");
+    }
+
+    @ApiOperation("删除指定id的问卷")
+    @PostMapping("/questionnaire/delete")
+    public Result removeQuestionnaire(@RequestBody String id) {
+        boolean b = questionnaireService.removeById(id);
+        return b ? Result.success("删除指定问卷成功") : Result.fail("删除指定问卷失败");
+    }
+}
