82926227aa707d267e7251e4f9d382cac5462e65
szy1840 <szy1840@gmail.com>
2022-11-01 17:06:28 +0800

    feat: 添加用户相关接口

--- src/main/java/com/starvey/controller/UserController.java
+++ src/main/java/com/starvey/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.starvey.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.starvey.common.Result;
+import com.starvey.entity.Questionnaire;
+import com.starvey.entity.User;
+import com.starvey.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.casbin.casdoor.config.CasdoorConfiguration;
 import org.casbin.casdoor.entity.CasdoorUser;
 import org.casbin.casdoor.service.CasdoorUserService;
 import org.casbin.casdoor.util.http.CasdoorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,47 +19,46 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author Yu yaodong
- * TODO: 由于和 casdoor 相关暂时没有改，包括接口格式，租户计费，是否需要从本地数据库CRUD等
- */
+
 @Api(tags = "用户管理")
 @RestController
 public class UserController {
 
-    @Resource
-    private CasdoorConfiguration casdoorConfiguration;
+    @Autowired
+    private UserService userService;
 
-    @Resource
-    private CasdoorUserService casdoorUserService;
-
-    @GetMapping("getUser")
-    public CasdoorUser getUser(String name) throws Exception {
-        return casdoorUserService.getUser(name);
+    @ApiOperation("根据租户id查询下属所有用户（分页）")
+    @GetMapping("/user")
+    public Result getUserOfTenant(@RequestParam String tenantId, @RequestParam(defaultValue = "1") Integer pageNumber, @RequestParam Integer pageSize) {
+        Page<User> page = userService.getUsersByTenantId(tenantId, pageNumber, pageSize);
+        return Result.success(page);
     }
 
-    @GetMapping("getUsers")
-    public List<CasdoorUser> getUsers() throws Exception {
-        return Arrays.asList(casdoorUserService.getUsers());
+    @ApiOperation("获取指定id的用户")
+    @GetMapping("/user/{id}")
+    public Result getUser(@PathVariable(name = "id") String id) {
+        User user = userService.getById(id);
+        return user != null ? Result.success(user) : Result. fail("获取用户失败，不存在该id的用户");
     }
 
-    @PostMapping("addUser")
-    public CasdoorResponse addUser(String name) throws IOException {
-        CasdoorUser user = new CasdoorUser();
-        user.setOwner(casdoorConfiguration.getOrganizationName());
-        user.setName(name);
-        user.setDisplayName(name);
-        return casdoorUserService.addUser(user);
+    @ApiOperation("添加用户")
+    @PostMapping("/user/add")
+    public Result addUser(@RequestBody User user) {
+        boolean b = userService.save(user);
+        return b ? Result.success(user) : Result.fail("添加用户失败");
     }
 
-    @PostMapping("updateUser")
-    public CasdoorResponse updateUser(CasdoorUser user) throws IOException {
-        return casdoorUserService.updateUser(user);
+    @ApiOperation("更新指定id的用户")
+    @PostMapping("/user/update")
+    public Result updateUser(@RequestBody User user) {
+        boolean b = userService.updateById(user);
+        return b ? Result.success(user) : Result.fail("更新指定用户失败");
     }
 
-    @DeleteMapping("deleteUser")
-    public CasdoorResponse deleteUser(String name) throws Exception {
-        CasdoorUser user = casdoorUserService.getUser(name);
-        return casdoorUserService.deleteUser(user);
+    @ApiOperation("删除指定id的用户")
+    @PostMapping("/user/delete")
+    public Result deleteUser(@RequestBody String id) {
+        boolean b = userService.removeById(id);
+        return b ? Result.success("删除指定用户成功") : Result.fail("删除指定用户失败");
     }
 }
